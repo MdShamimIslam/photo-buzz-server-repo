e329eb13ae5f80bbc469e2965d1dde6ab16197cf
MdShamimIslam <shamim401897@gmail.com>
2022-12-23 01:15:51 +0600

    get the reviews

--- index.js
+++ index.js
@@ -130,7 +130,7 @@ app.get("/reviews", async(req, res) => {
         })
         app.get('/reviewsEdit/:id', async (req, res) => {
           const id = req.params.id;
-          // console.log(id);
+          
             const query = { _id: ObjectId(id) };
         
          const result = await reviewCollection.findOne(query);
